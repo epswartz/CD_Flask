c24088b49c48870be0a025347426470697b37841
epswartz <eswartzen@gmail.com>
2021-01-25 21:06:18 -0500

    remove global usage

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-number = 1
 start_time = str(datetime.now())
 
 @app.route("/", methods=["GET"])
@@ -11,15 +10,11 @@ def main():
     """
     Handler for index.
     """
-    global number
-    global start_time
 
     resp = [
         "Hello world!",
-        f"You are visitor number: {number}",
         f"Start Time: {start_time}"
     ]
-    number += 1
     return "\n".join(resp)
 
 if __name__ == "__main__":
